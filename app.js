102d8a748eb4c23eff1f11a71ca2cf5c28ca226d
salmangenai <154249692+salmangenai@users.noreply.github.com>
2024-01-25 00:23:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+console.log ("Practise mathematical tables")
+document.write("mathematical tables<br>")
+var a=("practise")
+console.log(a)
+var a=34
+var b=39
+console.log (a+b)
+document.write(a*b + "<br>")
+alert("welcome to my world")
+ confirm("really want to proceed?")
+ var inp = prompt("please Enter the digit")
+   document.write(`${inp} x 1 = `, inp*1 + "<br>")
+   document.write(`${inp} x 2 = `, inp*2 + "<br>")
+   document.write(`${inp} x 3 = `, inp*3 + "<br>")
+   document.write(`${inp} x 4 = `, inp*4 + "<br>")
+   document.write(`${inp} x 5 = `, inp*5 + "<br>")
+   document.write(`${inp} x 6 = `, inp*6 + "<br>")
+   document.write(`${inp} x 7 = `, inp*7 + "<br>")
+   document.write(`${inp} x 8 = `, inp*8 + "<br>")
+   document.write(`${inp} x 9 = `, inp*9 + "<br>")
+   document.write(`${inp} x 10 = `, inp*10 + "<br>")
+   document.write(`${inp} x 11 = `, inp*11 + "<br>")
+   document.write(`${inp} x 12 = `, inp*12 + "<br>")
+   document.write(`${inp} x 13 = `, inp*13 + "<br>")
+   document.write(`${inp} x 14 = `, inp*14 + "<br>")
+   document.write(`${inp} x 15 = `, inp*15 + "<br>")
+   document.write(`${inp} x 16 = `, inp*16 + "<br>")
+   document.write(`${inp} x 17 = `, inp*17 + "<br>")
+   document.write(`${inp} x 19 = `, inp*19 + "<br>")
+   document.write(`${inp} x 20 = `, inp*20 + "<br>")
+ console.log
+  console.log(`${inp} x 1 = `, inp*1 + "<br>")
+  console.log(`${inp} x 2 = `, inp*2 + "<br>")
+  console.log(`${inp} x 3 = `, inp*3 + "<br>")
+  console.log(`${inp} x 4 = `, inp*4 + "<br>")
+  console.log(`${inp} x 5 = `, inp*5 + "<br>")
+  console.log(`${inp} x 6 = `, inp*6 + "<br>")
+  console.log(`${inp} x 7 = `, inp*7 + "<br>")
+  console.log(`${inp} x 8 = `, inp*8 + "<br>")
+  console.log(`${inp} x 9 = `, inp*9 + "<br>")
+  console.log(`${inp} x 10 = `, inp*10 + "<br>")
+
+const ptr = prompt("Tell me Your Percentage")
+
+if (ptr<= 40) {
+    console.log("Try Again")
+}  else if (ptr<= 50) {
+    console.log("Your got D grade")
+} else if(ptr<= 60){
+    console.log("you got C grade")
+}else if(ptr<= 70){
+    console.log("you got B grade")
+}else if(ptr<= 80){
+    console.log("you got A grade")
+}else if(ptr<= 100){
+    console.log("you got A-1 grade")
+}else{
+    console.log("Please give me correct data")
+}
